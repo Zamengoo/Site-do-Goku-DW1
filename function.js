45d2f151e977a5253bc2b609cf282dda22cf1443
Eduardo <163502115+Zamengoo@users.noreply.github.com>
2024-09-29 17:03:20 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -1,9 +1,10 @@
 function cilio() {
-    alert("Ciro o Calvo?")
+    alert("Ciro o Calvo?");
 }
 function bolso() {
-    alert("Bolsonaro é Norte Bolsonaro é Nordeste VAI 17 VAI 17!")
+    alert("Bolsonaro é Norte, Bolsonaro é Nordeste, VAI 17 VAI 17!");
 }
 function taxa() {
-    
+    alert("Lula la, seila quem aqui!");
 }
+let tax = "Haddad Taxou sua Ação.";
