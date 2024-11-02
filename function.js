bd6d6e4dd5cd064aa43e631229a063a05d00039c
Eduardo <163502115+Zamengoo@users.noreply.github.com>
2024-11-02 17:47:01 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -1,10 +1,26 @@
-function cilio() {
+function ciro() {
     alert("Ciro o Calvo?");
 }
-function bolso() {
+
+function bolsonaro() {
     alert("Bolsonaro é Norte, Bolsonaro é Nordeste, VAI 17 VAI 17!");
 }
-function taxa() {
+
+function petista() {
     alert("Lula la, seila quem aqui!");
 }
-let tax = "Haddad Taxou sua Ação.";
+
+function haddad() {
+    document.getElementById("lula").src = "img/Taxad.jpg";
+    document.getElementById("bolso").src = "img/Taxad.jpg";
+    document.getElementById("cilio").src = "img/Taxad.jpg";
+    document.getElementById("title").textContent = "TAXE HADDAD";
+    document.getElementById("cu").textContent = "A TAXA É REAL";
+    document.getElementById("cus").textContent = "A TAXA É REAL";
+    document.getElementById("cuz").textContent = "A TAXA É REAL";
+    document.getElementById("botaoL").textContent = "Taxad";
+    document.getElementById("botaoC").textContent = "Taxad";
+    document.getElementById("botaoB").innerHTML = "Taxad";
+    document.body.style.backgroundImage += "url('https://i.pinimg.com/564x/81/39/24/8139246cdc8cebbcfbd53cbdad3dc890.jpg')";
+    document.body.style.backgroundSize += 'cover';
+}
