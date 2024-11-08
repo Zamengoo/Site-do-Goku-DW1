56c5fb2717b3fef62163f43a8475a3631c4ad68f
Eduardo <163502115+Zamengoo@users.noreply.github.com>
2024-11-08 14:50:18 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -65,3 +65,57 @@ function taxa() {
         alert('Suas mãos não Aguentam mais Taxar.')
     }
 }
+
+function bolsoC() {
+    document.getElementById('bolso').style.color = 'rgb(36, 105, 2)';
+    document.getElementById('lula').style.color = 'rgb(36, 105, 2)';
+    document.getElementById('cilio').style.color = 'rgb(36, 105, 2)';
+    document.getElementById('cu').style.color = 'black';
+    document.getElementById('cus').style.color = 'black';
+    document.getElementById('cuz').style.color = 'black';
+    document.getElementById('title').style.color = 'white';
+    document.getElementById('cu').style.backgroundColor = 'darkgreen';
+    document.getElementById('cus').style.backgroundColor = 'darkgreen';
+    document.getElementById('cuz').style.backgroundColor = 'darkgreen';
+}
+
+function bolsoi() {
+    document.body.style.backgroundImage = 'url("img/Brasil.png")';
+    document.body.style.backgroundSize = '100%';
+    document.getElementById('lula').src = 'img/bosonaro.jpg';
+    document.getElementById('cilio').src = 'img/canalhas.jpg';
+    document.getElementById('bolso').src = 'img/luffy.jpg';
+    document.querySelector('link[rel="icon"]').href = 'img/22.png';
+    bolsoC()
+    bolsoT()
+}
+
+function bolsoT() {
+    document.getElementById('title').textContent = '22 É BOLSONARO 100% BRASIL';
+    document.getElementById('cu').textContent = 'BOLSONARO É NORTE';
+    document.getElementById('cus').textContent = 'BOLSONARO É NORDESTE';
+    document.getElementById('cuz').textContent = 'VAI 17! VAI 17!';
+    document.getElementById('botaoL').innerHTML = '<button onclick="destaxa()" " style="background-color: rgb(23, 36, 176); border:none; font-size:30px; font-family:Comic-Sans-MS; color: white;">Reduzir Taxas</button>';
+    document.getElementById('botaoC').innerHTML = '<button onclick="destaxa()" " style="background-color: rgb(230, 226, 39); border:none; font-size:30px; font-family:Comic-Sans-MS;">Reduzir Taxas</button>';
+    document.getElementById('botaoB').innerHTML = '<button onclick="destaxa()" " style="background-color: rgb(23, 36, 176); border:none; font-size:30px; font-family:Comic-Sans-MS; color: white;">Reduzir Taxas</button>';
+    document.getElementById('escolha').innerHTML = '';
+    document.title = 'TAXADO';
+}
+
+function destaxa() {
+    if(a == 1) {
+        alert('Taxas Reduzidas para 50%.')
+        a += 1
+    } else if(a == 2) {
+        alert('Taxas Reduzidas para 25%.')
+        a += 1
+    } else if (a == 3) {
+        alert('Taxas Reduzidas para 10%.')
+        a += 1
+    } else if (a == 4) {
+        alert('Taxas Reduzidas para 0%, Projeto Brazuela Derrubado.')
+        a += 1
+    } else {
+        alert('Não Existem Taxas para Reduzir.')
+    }
+}
